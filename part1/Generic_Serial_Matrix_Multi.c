85e251fa8b592979430a0d3514f625f3824edb75
Lucas Fioravante Ferreira <lucasfioravantef@gmail.com>
2025-09-27 14:36:56 -0300

    last_update exec1

--- part1/Generic_Serial_Matrix_Multi.c
+++ part1/Generic_Serial_Matrix_Multi.c
@@ -1,83 +1,176 @@
-//Sequential matrix multiplication for a generic matrix of generic size and 
-//fixed input of value 1. The changeable variable is the number of rows
-//which adjusts the number of columns too as rows = columns and 
-//produces a square matrix of sise specified. 
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
+#include <omp.h>
 #include <sys/time.h>
 
-//define the amount of rows which essentially sets the 
-//size of the matrix as the rows = columnns, therefore 
-//a square matrix is generated 
-#define ROWS 5
-#define COLUMNS ROWS
 
-//Initialising all three matrices 
-int MATRIX_A[ROWS][COLUMNS];
-int MATRIX_B[ROWS][COLUMNS];
-int MATRIX_C[ROWS][COLUMNS];
+// Parameters will be defined by Makefile compiler flags
+#ifndef PARALLEL
+#define PARALLEL 0 // Default to sequencial mode
+#endif
 
-//Function to fill matrix A and matrix B with value 1
-void Fill_Matrix(int mat[ROWS][COLUMNS]) { 
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLUMNS; j++)
-			mat[i][j] = 1;
+#ifndef REPETITIONS
+#define REPETITIONS 1 // Default repetitions
+#endif
+
+int fnbCheckMatrixResult(int **ppiMatrixA, int **ppiMatrixC, int iSize)
+{
+	for (int i = 0; i < iSize; i++)
+		for (int j = 0; j < iSize; j++)
+			if (ppiMatrixC[i][j] != ppiMatrixA[i][j])
+				return 0;
+	return 1;
+}
+
+void fnvAllocateMatrices(int iSize, int ***ppiMatrixA, int ***ppiMatrixB, int ***ppiMatrixC)
+{
+	*ppiMatrixA = malloc(iSize * sizeof(int *));
+	*ppiMatrixB = malloc(iSize * sizeof(int *));
+	*ppiMatrixC = malloc(iSize * sizeof(int *));
+
+	for (int i = 0; i < iSize; i++)
+	{
+		(*ppiMatrixA)[i] = malloc(iSize * sizeof(int));
+		(*ppiMatrixB)[i] = malloc(iSize * sizeof(int));
+		(*ppiMatrixC)[i] = calloc(iSize, sizeof(int));
 	}
 }
 
-//Function written to print out resulting matrix  
-void printMatrix(int mat[ROWS][COLUMNS]) { 
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLUMNS; j++)
-			printf("%d ", mat[i][j]);
-		printf("\n");
+void fnvFillMatrix(int iSize, int **ppiMatrix)
+{
+	for (int i = 0; i < iSize; i++)
+		for (int j = 0; j < iSize; j++)
+			ppiMatrix[i][j] = j + 1;
+}
+
+void fnvCreateIdentityMatrix(int iSize, int **ppiMatrix)
+{
+	for (int i = 0; i < iSize; i++)
+		for (int j = 0; j < iSize; j++)
+			ppiMatrix[i][j] = (i == j) ? 1 : 0;
+}
+
+void fnvZeroFillMatrix(int iSize, int **ppiMatrix)
+{
+	for (int i = 0; i < iSize; i++)
+		for (int j = 0; j < iSize; j++)
+			ppiMatrix[i][j] = 0;
+}
+
+void fnvMultiplyMatrices(int iSize, int **ppiMatrixA, int **ppiMatrixB, int **ppiMatrixC)
+{
+// Matrix multiplication: C = A × B
+#pragma omp parallel for collapse(2) if (PARALLEL)
+	for (int i = 0; i < iSize; i++)
+		for (int j = 0; j < iSize; j++)
+			for (int k = 0; k < iSize; k++)
+				ppiMatrixC[i][j] += ppiMatrixA[i][k] * ppiMatrixB[k][j];
+}
+
+void fnvFreeMatrices(int iSize, int **ppiMatrixA, int **ppiMatrixB, int **ppiMatrixC)
+{
+	for (int i = 0; i < iSize; i++)
+	{
+		free(ppiMatrixA[i]);
+		free(ppiMatrixB[i]);
+		free(ppiMatrixC[i]);
 	}
+	free(ppiMatrixA);
+	free(ppiMatrixB);
+	free(ppiMatrixC);
 }
 
 int main(int argc, char *argv[])
 {
-	//creates a composite group to physically place
-	//different data types under one group
-	struct timeval t1, t2;
-	//creates a variable for the elapsed time 
-	//with data type double  
-	double Elapsed_Time;
-	//Fill matrix A and matrix B using the above function 
-	Fill_Matrix(MATRIX_A);
-	Fill_Matrix(MATRIX_B);
-    
-    //start timer
-	gettimeofday(&t1, NULL); 
-    
-    //multiplication of the 2 input matrices 
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j <COLUMNS; j++) {
-			for (int k = 0; k <COLUMNS; k++) 
-				MATRIX_C[i][j] += MATRIX_A[i][k] * MATRIX_B[k][j];
-			}
-		}
-	
-	//stop timer 
-	gettimeofday(&t2, NULL); 
+	if (argc != 3)
+	{
+		printf("Usage: %s <iMatrixSize> <iThreadCount>\n", argv[0]);
+		printf("Compile-time settings: PARALLEL=%d, REPETITIONS=%d\n", PARALLEL, REPETITIONS);
+		return 1;
+	}
+#if !PARALLEL
 
-    // Compute and print the elapsed time in milli seoonds. 
-	Elapsed_Time = (t2.tv_sec - t1.tv_sec)*1000.0; 
-	Elapsed_Time += (t2.tv_usec - t1.tv_usec)/1000.0; 
+#endif
+	int iSize = atoi(argv[1]);
+#if PARALLEL
+	int iThreads = atoi(argv[2]);
+#else
+		struct timeval t1, t2;
+#endif
+	double dStartTime, dEndTime;
+	double dTotalTime = 0.0;
+	double dMinTime = 99999999999;
+	double dMaxTime = 0.0;
 
+	int **ppiMatrixA, **ppiMatrixB, **ppiMatrixC;
+#if PARALLEL
+	omp_set_num_threads(iThreads);
+#endif
+#if PARALLEL
+	printf("=== PARALLEL MODE (%d iThreads) ===\n", iThreads);
+#else
+	printf("=== SEQUENTIAL MODE ===\n");
+#endif
 
-    //print statements to print each matrix, the resultant
-    //matrix and the elasped time of the program. 
-	printf("Matrix A is: \n");
-    printMatrix(MATRIX_A);
-    printf("Matrix B is: \n");
-    printMatrix(MATRIX_B);
-    printf("Resultant Matrix C, where Matrix A X Matrix B is: \n");
-    printMatrix(MATRIX_C);
-    
-    printf("The elapsed time is %f ms \n", Elapsed_Time); 
+	printf("Repetitions: %d\n", REPETITIONS);
+	printf("Matrix size: %dx%d\n", iSize, iSize);
+
+	fnvAllocateMatrices(iSize, &ppiMatrixA, &ppiMatrixB, &ppiMatrixC);
+	fnvFillMatrix(iSize, ppiMatrixA);
+	fnvCreateIdentityMatrix(iSize, ppiMatrixB);
+
+	// Multiple executions for average calculation
+	for (int i = 0; i < REPETITIONS; i++)
+	{
+		fnvZeroFillMatrix(iSize, ppiMatrixC);
+
+#if PARALLEL
+		dStartTime = omp_get_wtime();
+#else
+		gettimeofday(&t1, NULL); 
+#endif
+
+		fnvMultiplyMatrices(iSize, ppiMatrixA, ppiMatrixB, ppiMatrixC);
+
+#if PARALLEL
+		dEndTime = omp_get_wtime();
+		double dCurrentTime = (dEndTime - dStartTime) * 1000.0;
+#else
+		gettimeofday(&t2, NULL);
+		double dCurrentTime = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
+#endif
+
+		dTotalTime += dCurrentTime;
+
+		if (dCurrentTime < dMinTime)
+			dMinTime = dCurrentTime;
+		if (dCurrentTime > dMaxTime)
+			dMaxTime = dCurrentTime;
+
+		printf("Execution %2d: %f ms\n", i + 1, dCurrentTime);
+	}
+#if PARALLEL
+	// Result verification (only on last execution)
+	printf("Verifying result...\n");
+	if (fnbCheckMatrixResult(ppiMatrixA, ppiMatrixC, iSize))
+	{
+		printf("✓ Result CORRECT! A × I = A\n");
+	}
+	else
+	{
+		printf("✗ Result INCORRECT!\n");
+	}
+#endif
+	// Statistics
+	double dAverageTime = dTotalTime / REPETITIONS;
+	printf("\n=== STATISTICS ===\n");
+	printf("Average time: %f ms\n", dAverageTime);
+	printf("Minimum time: %f ms\n", dMinTime);
+	printf("Maximum time: %f ms\n", dMaxTime);
+	printf("Variation: ±%f ms\n", (dMaxTime - dMinTime) / 2);
+	printf("Speedup (min/max): %.2fx\n", dMaxTime / dMinTime);
+
+	fnvFreeMatrices(iSize, ppiMatrixA, ppiMatrixB, ppiMatrixC);
+
 	return 0;
 }
-
